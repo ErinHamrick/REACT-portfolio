974a09556e808c5eb9b1f9d383ec2ba910f5a41f
Erin Hamrick <erin.hamrick76@gmail.com>
2023-11-21 13:33:25 -0600

    removed Image component.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import About from '../components/About';
-import Image from '../components/Image';
+
 
 function HomePage() {
 	return (
 		<div>
-			<Image />
 			<About />
-			{/* Add other sections or components as needed */}
 		</div>
 	);
 }
