b2a966381d34e5bd6a06344d2de7208e0353698f
Erin Hamrick <erin.hamrick76@gmail.com>
2023-11-21 13:56:41 -0600

    reconfigured vite.config.js to create a build directory.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true
+//   }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Set the output directory for the build
+const outDir = 'build';
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: outDir,
+  },
   server: {
     port: 3000,
-    open: true
-  }
-})
+    open: true,
+  },
+});
